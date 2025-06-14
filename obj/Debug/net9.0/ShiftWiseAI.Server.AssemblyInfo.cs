8cd61fbd71fc2dbe34c4b1f2c4712d7c49f627b5
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2025-06-14 19:42:13 +0530

    file update

--- obj/Debug/net9.0/ShiftWiseAI.Server.AssemblyInfo.cs
+++ obj/Debug/net9.0/ShiftWiseAI.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShiftWiseAI.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69c5f7b95f63d5e1ef8717b37e940ce780e77bf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a20ad950f2a7011dabca835603f0391e6677f65")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShiftWiseAI.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShiftWiseAI.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
