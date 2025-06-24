0473ea73c4dab2d0e629c9e920e8ec9f6105975d
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2025-06-25 00:16:40 +0530

    Bulk employee creation developed

--- DTOs/UpdateEmployeeRequest.cs
+++ DTOs/UpdateEmployeeRequest.cs
@@ -3,6 +3,7 @@
     public class UpdateEmployeeRequest
     {
         public string FullName { get; set; }
+        public string Email { get; set; }
         public string Role { get; set; }
         public string Phone { get; set; }
         public int MaxWeeklyHours { get; set; }
